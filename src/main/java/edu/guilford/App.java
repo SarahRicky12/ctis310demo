e60bc57a64bd2b36af25ef55c4aec6970ba57737
SarahRicky12 <ssheets@guilford.edu>
2023-04-20 18:31:01 -0400

    Dogs Commit

--- src/main/java/edu/guilford/App.java
+++ src/main/java/edu/guilford/App.java
@@ -1,16 +1,33 @@
 package edu.guilford;
 
+import java.util.ArrayList;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) {
+        Dogs dog1 = new Dogs("Fido", "Poodle", 3, 20.5);
+        ArrayList<Dogs> dogList = new ArrayList<Dogs>();
+        dogList.add(dog1);
+
+        // Create a loop to instantiate 5 dogs and add them to the ArrayList
+
+        for (int i = 0; i < 5; i++) {
+            Dogs dog = new Dogs("Name", "Breed");
+            dogList.add(dog);
+        }
+
+        // Create a loop to print out the name and breed of each dog in the ArrayList
+
+        for (Dogs dog : dogList) {
+            System.out.println(dog.getName() + " " + dog.getBreed() + " " + dog.getAge() + " " + dog.getWeight());
+        }
+
+        // Create a loop to analyze each dogs weight using weightAnalysis() method
+
+        for (Dogs dog : dogList) {
+            dog.weightAnalysis();
+        }
+
     }
 
-    
 }
